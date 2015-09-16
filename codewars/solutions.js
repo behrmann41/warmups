2050611f53c3c38ce8971616758b072c3fe1d8d5
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-16 13:32:14 -0600

    codewar problem complete

--- codewars/solutions.js
+++ codewars/solutions.js
@@ -202,3 +202,26 @@ function array_diff(a, b) {
 function array_diff(a, b) {
   return a.filter(function(elem) { return b.indexOf(elem) == -1; });
 }
+
+// chaining functions
+
+function f1(x){ return x*2 }
+function f2(x){ return x+2 }
+function f3(x){ return Math.pow(x,2) }
+
+function f4(x){ return x.split("").concat().reverse().join("").split(" ")}
+function f5(xs){ return xs.concat().reverse() }
+function f6(xs){ return xs.join("_") }
+
+//solution
+function chained(functions) {
+  return function(input){
+    return functions.reduce(function(input, fn){ return fn(input) }, input);
+  }
+}
+
+chained([f1,f2,f3])(0) // -> 4
+chained([f1,f2,f3])(2) // -> 36
+chained([f3,f2,f1])(2) // -> 12
+
+chained([f4,f5,f6])("lorem ipsum") // -> "merol_muspi"
