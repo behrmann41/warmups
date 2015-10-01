8b99c65ff04c25ce8d9cacb2a9048a55be48add0
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-01 09:43:39 -0600

    DiagonalSum problem

---
+++ diagonalSum.js
@@ -0,0 +1,21 @@
+// my initial solution
+
+var exampleArray =
+
+                  [[1, 0, 0, 0],
+
+                  [0, 1, 0, 0],
+
+                  [0, 0, 1, 0],
+
+                  [0, 0, 0, 1]]
+
+function diagonalSum(array){
+   var count = 0;
+   for (var i = 0; i < array.length; i++){
+       count += array[i][i]
+   }
+   return count;
+}
+              
+diagonalSum(exampleArray) // => 4
