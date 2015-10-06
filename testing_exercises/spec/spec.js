34bdb11351be2f8cdcc4bab8e6ad64a65828c324
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-06 09:39:31 -0600

    added additional test for edge case

--- testing_exercises/spec/spec.js
+++ testing_exercises/spec/spec.js
@@ -4,5 +4,6 @@ describe('largestProduct', function() {
   it("returns the largest product of any two integers in an array", function() {
       expect(code.largestProduct([2,3,4,-5,92,100])).toEqual(9200);
       expect(code.largestProduct([-2,3,-4,-5,-92,-100])).toEqual(9200);
+      expect(code.largestProduct([-2,3,-4,-5,-92,100])).toEqual(460)
   });
 });
